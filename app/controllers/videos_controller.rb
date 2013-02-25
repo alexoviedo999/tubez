50d218e7bc32979f08484b7a36ea470c52e73f1c
alexoviedo999 <alexoviedo999@gmail.com>
2013-02-25 15:55:29 -0500

    Init

---
+++ app/controllers/videos_controller.rb
@@ -0,0 +1,24 @@
+require 'open-uri'
+class VideosController < ApplicationController
+  def index
+    @videos = Video.all
+  end
+
+  def new
+  end
+
+  def search
+    search = params[:video]
+    url = "http://youtube.com/results?search_query=#{search}"
+    html = Nokogiri::HTML(open(url))
+    lis = html.css('#search-results li')
+    lis.each do |li|
+      url = li.get_attribute('get-content-item-id')
+      # views = li.get_attribute('get-content-item-id')
+      title = li.get_attribute('get-content-item-id')
+      Video.create(:title => title, :url => url)
+    end
+
+    redirect_to(videos_path)
+  end
+end
